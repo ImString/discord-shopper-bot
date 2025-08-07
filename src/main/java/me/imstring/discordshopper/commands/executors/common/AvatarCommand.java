8c215c061acef42c59ba675920cb0417384a6759
String <novadeveloper11@gmail.com>
2025-08-07 12:32:45 -0300

    feat: Add avatar command and add check permission in ping command.

---
+++ src/main/java/me/imstring/discordshopper/commands/executors/common/AvatarCommand.java
@@ -0,0 +1,45 @@
+package me.imstring.discordshopper.commands.executors.common;
+
+import me.imstring.discordshopper.Core;
+import me.imstring.discordshopper.commands.DiscordAbstractCommand;
+import me.imstring.discordshopper.configuration.ApplicationConfig;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public class AvatarCommand extends DiscordAbstractCommand {
+    public AvatarCommand() {
+        super("avatar", "Confira o avatar de um usuário.");
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return List.of(
+                new OptionData(
+                        OptionType.USER,
+                        "user",
+                        "Insira o usuário",
+                        false
+                )
+        );
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event, Core instance) {
+        User target = event.getOption("user") != null ? event.getOption("user").getAsUser() : event.getUser();
+        String avatarUrl = target.getEffectiveAvatarUrl();
+
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor("Avatar de " + target.getEffectiveName());
+        embed.setImage(avatarUrl);
+        embed.setColor(instance.getEmbedDefaultColor());
+        embed.setFooter("© " + ApplicationConfig.NAME, instance.getJda().getSelfUser().getEffectiveAvatarUrl());
+
+        event.replyEmbeds(embed.build()).queue();
+    }
+}
--- src/main/java/me/imstring/discordshopper/commands/executors/staff/PingCommand.java
+++ src/main/java/me/imstring/discordshopper/commands/executors/staff/PingCommand.java
@@ -1,13 +1,17 @@
-package me.imstring.discordshopper.commands.executors;
+package me.imstring.discordshopper.commands.executors.staff;
 
 import me.imstring.discordshopper.Core;
 import me.imstring.discordshopper.commands.DiscordAbstractCommand;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PingCommand extends DiscordAbstractCommand {
     public PingCommand() {
-        super("ping", "Verifique se o bot está online.");
+        super("ping", "Verifique se o bot está online.", new ArrayList<>(Collections.singleton(Permission.ADMINISTRATOR)));
     }
 
     @Override
@@ -15,7 +19,7 @@ public class PingCommand extends DiscordAbstractCommand {
         long start = System.currentTimeMillis();
         event.reply("Ping...").queue(success -> {
             long ping = System.currentTimeMillis() - start;
-            success.editOriginal("Pong! O ping é: " + ping + "ms | websocket: " + event.getJDA().getGatewayPing() + "ms").queue();
+            success.editOriginal("Pong \uD83C\uDFD3! O ping é: ``" + ping + "ms`` | WebSocket: ``" + event.getJDA().getGatewayPing() + "ms``").queue();
         }, failure -> {
             event.reply("Ocorreu um erro ao calcular o ping.").setEphemeral(true).queue();
         });
